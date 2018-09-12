1f3c23f0e228f4b92bb0c6c9ff4389cadc58d42e
Monica Tamaru Jones <mntamaru@gmail.com>
2018-09-12 16:09:37 +0000

    automatically backed up by learn

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -40,6 +40,6 @@ private
 
   # Never trust parameters from the scary internet, only allow the white list through.
   def post_params
-    params.require(:post).permit(:title, :description)
+    params.require(:post).permit(:title, :description, :author_id)
   end
 end
